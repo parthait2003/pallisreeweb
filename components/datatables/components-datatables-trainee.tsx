2fc9b395438bef587fcc011d475c638a3ee9ccd6
parthait2003 <customer@techpenta.com>
2024-08-26 11:52:17 +0530

    Update components-datatables-trainee.tsx

--- components/datatables/components-datatables-trainee.tsx
+++ components/datatables/components-datatables-trainee.tsx
@@ -1262,7 +1262,7 @@ const ComponentsDatatablesTrainee = () => {
                                   onChange={handleChange}
                                   className="form-input"
                                   value={formData.phoneno}
-                                  maxLength={10}
+                                   maxLength={10}
                                   required
                                 />
                               </div>
