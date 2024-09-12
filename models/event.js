4cad2a4c0a54f26d191ea83a09d2cca8dbded709
parthait2003 <customer@techpenta.com>
2024-09-12 19:43:08 +0530

    Update event.js

--- models/event.js
+++ models/event.js
@@ -7,11 +7,11 @@ const eventSchema = new mongoose.Schema({
     enum: ['Camp', 'Tournament', 'Notice'], // Restrict to known event types
   },
   date: {
-    type: Date,
+    type: String,
     required: true,
   },
   time: {
-    type: Date,
+    type: String,
     required: true,
   },
   traineeIds: [
